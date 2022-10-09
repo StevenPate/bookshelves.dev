bbb1dd5bfd636d958dfd5dac405fd405fe96ce89
StevenPate <stevenpate@gmail.com>
2022-10-08 22:47:54 -0700

    clean up a bit of scraping

--- src/js/_getData.js
+++ src/js/_getData.js
@@ -192,57 +192,69 @@ const getOpenLibrary = async (ISBN) => {
 // };
 
 const getAmazon = async (ISBN) => {
-    let coverUrl;
-    try {
-        let amznURL = `https://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-isbn=${ISBN}&sort=relevanceexprank`;
+  let coverUrl;
+  try {
+    let amznURL = `https://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-isbn=${ISBN}&sort=relevanceexprank`;
 
-        const amznData = await EleventyFetch(amznURL, {
-            type: "text",
-            duration: "10d",
-            directory: "_cache",
+    const amznData = await EleventyFetch(amznURL, {
+      type: "text",
+      duration: "100d", //lol
+      directory: "_cache",
+    });
+    const amznHTML = cheerio.load(amznData);
+    const amznImages = amznHTML("img.s-image").attr("srcset");
+    if (amznImages == null) {
+      console.log(`No amznImages data found for ${ISBN} at ${amznURL}.`);
+      return;
+    }
+
+    function parseSrcset(srcset) {
+      if (!srcset) return null;
+      return srcset
+        .split(", ")
+        .map((d) => d.split(" "))
+        .reduce((p, c) => {
+          if (c.length !== 2) {
+            // throw new Error("Error parsing srcset.");
+            return;
+          }
+          p[c[1]] = c[0];
+          return p;
+        }, {});
+    }
+    const parsedAmznImages = parseSrcset(amznImages);
+
+    if (parsedAmznImages == null) {
+      console.log(`No parsedAmznImages data found for ${ISBN} at ${amznURL}.`);
+      return;
+    }
+
+    let sortedAmznImages = [];
+    Object.keys(parsedAmznImages)
+      .sort()
+      .reverse()
+      .forEach((key) => {
+        sortedAmznImages.push({
+          size: key,
+          URL: parsedAmznImages[key],
         });
-        const amznHTML = cheerio.load(amznData);
-        const amznImages = amznHTML(".s-image").attr("srcset");
-        function parseSrcset(srcset) {
-            if (!srcset) return null;
-            return srcset
-                .split(", ")
-                .map((d) => d.split(" "))
-                .reduce((p, c) => {
-                    if (c.length !== 2) {
-                        // throw new Error("Error parsing srcset.");
-                        return;
-                    }
-                    p[c[1]] = c[0];
-                    return p;
-                }, {});
-        }
-        const parsedAmznImages = parseSrcset(amznImages);
-        let sortedAmznImages = [];
-        Object.keys(parsedAmznImages)
-            .sort()
-            .reverse()
-            .forEach((key) => {
-                sortedAmznImages.push({
-                    size: key,
-                    URL: parsedAmznImages[key],
-                });
-            });
-        coverUrl = sortedAmznImages[0].URL;
-    } catch (err) {
-        console.log(`getAmazon has a problem with ${ISBN} .`);
-        console.log(err);
-    }
-    let cachedCoverUrl;
-    if (coverUrl != null) {
-        cachedCoverUrl = await cacheImage(
-            coverUrl,
-            "book-cover not-prose my-0 transition duration-300 ease-in-out delay-50 border border-gray-100 hover:bg-white shadow hover:shadow-xl hover:-translate-y-1 hover:scale-110",
-            ISBN
-        );
-    }
-   
-    return { coverUrl, cachedCoverUrl };
+      });
+    coverUrl = sortedAmznImages[0].URL;
+
+  } catch (err) {
+    console.log(`getAmazon has a problem with ${ISBN} .`);
+    console.log(err);
+  }
+  let cachedCoverUrl;
+  if (coverUrl != null) {
+    cachedCoverUrl = await cacheImage(
+      coverUrl,
+      "book-cover not-prose my-0 transition duration-300 ease-in-out delay-50 border border-gray-100 hover:bg-white shadow hover:shadow-xl hover:-translate-y-1 hover:scale-110",
+      ISBN
+    );
+  }
+
+  return { coverUrl, cachedCoverUrl };
 };
 
 const getLibro = async (ISBN) => {
